4c8ec04c749ee0bc6b3e9a839e8cfd2da26c5d96
Fatih Yapıcı <fa.yapici@gmail.com>
2023-06-08 02:10:30 +0300

    update delete user comment-const-rule

--- ToDoWebApi/Applications/UserOperations/Commands/DeleteUser/DeleteUserCommand.cs
+++ ToDoWebApi/Applications/UserOperations/Commands/DeleteUser/DeleteUserCommand.cs
@@ -4,7 +4,7 @@ namespace ToDoWebApi.Applications.UserOperations.Commands.DeleteUser
 {
     public class DeleteUserCommand
     {
-        public const string ExceptionMessageFound = "User does not exist.";
+        public const string ExceptionMessageNotFound = "User does not exist.";
         public const string ExceptionMessageEmail = "User E-Mail is wrong.";
         public const string ExceptionMessagePassword = "User Password is wrong.";
 
@@ -19,23 +19,17 @@ namespace ToDoWebApi.Applications.UserOperations.Commands.DeleteUser
 
         public void Handle()
         {
-            // var user = _dbContext.Users.SingleOrDefault(x => x.Id == Model.Id);
-            // if (user is null)
-            //     throw new InvalidOperationException(ExceptionMessageFound);
-            // if (user.Email != Model.Email)
-            //     throw new InvalidOperationException(ExceptionMessageEmail);
-            // if (user.Password != Model.Password)
-            //     throw new InvalidOperationException(ExceptionMessagePassword);
-            // _dbContext.Users.Remove(user);
-            // _dbContext.SaveChanges();
-
             var user = _dbContext.Users.Find(Model.Id);
+
             if (user is null)
-                throw new InvalidOperationException(ExceptionMessageFound);
+                throw new InvalidOperationException(ExceptionMessageNotFound);
+
             if (user.Email != Model.Email)
                 throw new InvalidOperationException(ExceptionMessageEmail);
+
             if (user.Password != Model.Password)
                 throw new InvalidOperationException(ExceptionMessagePassword);
+                
             _dbContext.Users.Remove(user);
             _dbContext.SaveChanges();
         }
--- ToDoWebApi/Applications/UserOperations/Commands/DeleteUser/DeleteUserCommandValidator.cs
+++ ToDoWebApi/Applications/UserOperations/Commands/DeleteUser/DeleteUserCommandValidator.cs
@@ -6,6 +6,7 @@ namespace ToDoWebApi.Applications.UserOperations.Commands.DeleteUser
     {
         public DeleteUserCommandValidator()
         {
+            RuleFor(command => command.Model.Id).GreaterThan(0);
             RuleFor(command => command.Model.Email).NotEmpty().EmailAddress();
             RuleFor(command => command.Model.Password).NotEmpty().MinimumLength(6);
         }
