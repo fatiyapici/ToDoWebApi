03de6d1a477af8c8029a394139a2c4b7df13cfce
Fatih Yapıcı <fa.yapici@gmail.com>
2023-06-08 02:09:19 +0300

    update create user command validator

--- ToDoWebApi/Applications/UserOperations/Commands/CreateUser/CreateUserCommandValidator.cs
+++ ToDoWebApi/Applications/UserOperations/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -6,8 +6,8 @@ namespace ToDoWebApi.Applications.UserOperations.Commands.CreateUser
     {
         public CreateUserCommandValidator()
         {
-            RuleFor(command => command.Model.Name).NotEmpty();
-            RuleFor(command => command.Model.Surname).NotEmpty();
+            RuleFor(command => command.Model.Name).NotEmpty().MinimumLength(2);
+            RuleFor(command => command.Model.Surname).NotEmpty().MinimumLength(2);
             RuleFor(command => command.Model.Email).NotEmpty().EmailAddress();
             RuleFor(command => command.Model.Password).NotEmpty().MinimumLength(6);
         }
