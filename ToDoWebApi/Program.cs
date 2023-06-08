8c86abc35887ceb5996c4ff6898e24b6e547dd48
Fatih Yapıcı <fa.yapici@gmail.com>
2023-06-08 22:01:55 +0300

    token config

--- ToDoWebApi/Program.cs
+++ ToDoWebApi/Program.cs
@@ -1,5 +1,8 @@
 using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using ToDoWebApi.DbOperations;
 using ToDoWebApi.DBOperations;
@@ -7,6 +10,25 @@ using ToDoWebApi.DBOperations;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = configuration["Token:Issuer"],
+            ValidAudience = configuration["Token:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Token:SecurityKey"])),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +37,7 @@ builder.Services.AddSwaggerGen(c =>
     {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
     });
+
 builder.Services.AddDbContext<ToDoDbContext>(options => options.UseInMemoryDatabase(databaseName: "ToDoDB"));
 builder.Services.AddScoped<IToDoDbContext, ToDoDbContext>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
@@ -28,6 +51,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
 }
+
+app.UseAuthentication();
 
 app.UseHttpsRedirection();
 
