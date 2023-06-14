df7e7527944d0253f84ecea78b0774ee0c937ecd
Fatih Yapıcı <fa.yapici@gmail.com>
2023-06-14 16:19:05 +0300

    update card

--- ToDoWebApi/Entity/Card.cs
+++ ToDoWebApi/Entity/Card.cs
@@ -6,17 +6,10 @@ namespace ToDoWebApi.Entity
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        public int UserId { get; set; }
         public string Name { get; set; }
         public Status Status { get; set; }
-        public virtual ICollection<UserCard> UserCards { get; set; }
-    }
-
-    public class UserCard
-    {
-        public int UserId { get; set; }
         public User User { get; set; }
-        public int CardId { get; set; }
-        public Card Card { get; set; }
     }
 
     public enum Status
