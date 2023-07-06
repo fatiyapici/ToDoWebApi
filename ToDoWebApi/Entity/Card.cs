ab5b75c4f297fdfd83c41a981fea58a62764a94d
Fatih Yapıcı <fa.yapici@gmail.com>
2023-07-06 23:55:35 +0300

    update card entity lining arrangement

--- ToDoWebApi/Entity/Card.cs
+++ ToDoWebApi/Entity/Card.cs
@@ -7,9 +7,9 @@ namespace ToDoWebApi.Entity
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public int UserId { get; set; }
+        public User User { get; set; }
         public string Name { get; set; }
         public Status Status { get; set; }
-        public User User { get; set; }
     }
 
     public enum Status
